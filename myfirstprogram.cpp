27fa5f87f57ff95057bd3af083b6d2b029d658da
yonghoow <2073951b@student.tp.edu.sg>
2025-03-24 23:20:31 +0800

    updated 24 Mar

---
+++ myfirstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*The code below prints have a good day
+and it is amazing */
+int main() {
+  int myNum = 15;
+  myNum = 10;
+  cout << myNum << "\n";
+  cout << "Have a good day too!";
+  return 0;
+}
