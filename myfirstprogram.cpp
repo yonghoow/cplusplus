ef77e588dd4f845fbd0cb4203c14b5a876d9b7c7
yonghoow <2073951b@student.tp.edu.sg>
2025-03-24 23:35:47 +0800

    updated 24 Mar

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
 
-/*The code below prints have a good day
+/*The code below prints what I want to say
 and it is amazing */
 int main() {
-  int myNum = 15;
-  myNum = 10;
-  cout << myNum << "\n";
-  cout << "Have a good day too!";
-  return 0;
+    //Student data
+    int studentID = 15;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    //Print variable
+    cout << "Student ID: " << studentID << "\n";
+    cout << "Student Age: " << studentAge << "\n";
+    cout << "Student Fee: " << studentFee << "\n";
+    cout << "Student Grade: " << studentGrade << "\n";
+
+    return 0;
 }
